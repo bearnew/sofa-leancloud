882d7b2bc9442f5845c111b9d4afbdcc4f0da37b
xx熊新 <xiong_x@Ctrip.com>
2020-03-04 19:48:37 +0800

    update

--- server/app.js
+++ server/app.js
@@ -18,24 +18,51 @@ const app = new Koa();
 app.use(views(path.join(__dirname, '../dist')));
 
 // 设置静态资源目录
-app.use(statics(path.join(__dirname, '../dist')));
+// app.use(statics(path.join(__dirname, '../dist')));
 
 app.use(async (ctx, next) => {
     ctx.set("Access-Control-Allow-Origin", "http://localhost:2222");
     await next();
 });
 
-const router = new Router();
-app.use(router.routes());
+var count = 1;
+var maxAllowRequest = 5;
+const queue = [];
+
+console.log('--------');
+app.use(async (ctx, next) => {
+    // 如果超过了最大并发数目
+    console.log(count);
+    if (count >= maxAllowRequest) {
+        // 如果当前队列中已经过长
+        await new Promise((resolve, reject) => {
+            queue.push(resolve);
+        });
+    }
+    console.log('+++++++');
+    count++;
+    console.log('4444444');
+    await next();
+    count--;
+    const task = queue.shift();
+    task && task();
+});
 
 // 加载云引擎中间件
 app.use(AV.koa());
 
 app.use(bodyParser());
 
+const router = new Router();
+app.use(router.routes());
+
+
 router.get('/', async function (ctx) {
     ctx.state.currentTime = new Date();
-    await ctx.render('./index.html');
+    console.log('9999');
+    // await ctx.render('./index.html');
+    await delay();
+    ctx.body = 'hello koa';
 });
 
 // 可以将一类的路由单独保存在一个文件中
@@ -43,5 +70,12 @@ app.use(require('./routes/todos').routes());
 app.use(require('./routes/store').routes());
 app.use(require('./routes/user').routes());
 
-
 module.exports = app;
+
+function delay() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 1000);
+    });
+}
